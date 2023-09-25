8f80cdabf5b549555c705bc0a48413d2ec299b95
Lena <pjanvalena333@gmail.com>
2023-09-25 23:54:47 +0500

    init test

---
+++ buns/mod1/src/main/java/com/example/mod1/spring_homework/homework1/Pet.java
@@ -0,0 +1,5 @@
+package com.example.mod1.spring_homework.homework1;
+
+public interface Pet {
+    public void say();
+}
---
+++ buns/mod1/src/main/java/com/example/mod1/spring_homework/homework1/Test1.java
@@ -0,0 +1,8 @@
+package com.example.mod1.spring_homework.homework1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Pet pet = new Dog();
+        pet.say();
+    }
+}
