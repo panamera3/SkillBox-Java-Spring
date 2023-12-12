b277125c5d5c215f87364a1a76abde9fe6b90b6c
Lena <pjanvalena333@gmail.com>
2023-12-12 20:38:56 +0500

    bruh

---
+++ skillbox/mod8/src/main/java/com/example/MyBookShopApp/data/service/BookService.java
@@ -0,0 +1,127 @@
+package com.example.MyBookShopApp.data.service;
+
+import com.example.MyBookShopApp.data.book.Book;
+import com.example.MyBookShopApp.data.book.Tag;
+import com.example.MyBookShopApp.data.repo.BookRepo;
+import com.example.MyBookShopApp.data.repo.TagRepo;
+import com.example.MyBookShopApp.errs.BookstoreApiWrongParameterException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepo bookRepository;
+    private final TagRepo tagRepo;
+    private final BooksRatingAndPopulatityService booksRatingAndPopulatityService;
+
+    @Autowired
+    public BookService(BookRepo bookRepositorysitory, TagRepo tagRepo, BooksRatingAndPopulatityService booksRatingAndPopulatityService) {
+        this.bookRepository = bookRepositorysitory;
+        this.tagRepo = tagRepo;
+        this.booksRatingAndPopulatityService = booksRatingAndPopulatityService;
+    }
+
+    public List<Book> getBooksData(){
+        return bookRepository.findAll();
+    }
+    //NEW BOOK SERVICE METHODS
+
+    public List<Book> getBooksByAuthor(String authorName){
+        return bookRepository.findBooksByAuthorFirstNameContaining(authorName);
+    }
+
+    public List<Book> getBooksByTitle(String title) throws BookstoreApiWrongParameterException {
+        if(title.equals("") || title.length()<=1){
+            throw new BookstoreApiWrongParameterException("Wrong values passed to one or more parameters");
+        }else{
+            List<Book> data = bookRepository.findBooksByTitleContaining(title);
+            if(data.size() > 0){
+                return data;
+            }else {
+                throw new BookstoreApiWrongParameterException("No data found with specified parameters...");
+            }
+        }
+    }
+
+    public List<Book> getBooksWithPriceBetween(Integer min, Integer max){
+        return bookRepository.findBooksByPriceBetween(min, max);
+    }
+
+    public List<Book> getBooksWithPrice(Integer price){
+        return bookRepository.findBooksByPriceIs(price);
+    }
+
+    public List<Book> getBooksWithMaxPrice(){
+        return bookRepository.getBooksWithMaxDiscount();
+    }
+
+    public List<Book> getBestsellers(){
+        return bookRepository.getBestsellers();
+    }
+
+    public Page<Book> getPageofRecommendedBooks(Integer offset, Integer limit){
+        Pageable nextPage = PageRequest.of(offset,limit);
+        return bookRepository.findAll(nextPage);
+    }
+
+    public Page<Book> getPageofRecentBooks(Integer offset, Integer limit) {
+        Pageable nextPage = PageRequest.of(offset, limit);
+        return bookRepository.findAll(nextPage);
+    }
+
+    public Page<Book> getPageofRecentBooks(Integer offset, Integer limit, Date from, Date to){
+        Pageable nextPage = PageRequest.of(offset,limit, Sort.by("pub_date").ascending());
+        return bookRepository.findBooksByDateBetween(from, to, nextPage);
+    }
+
+    public Page<Book> getPageofPopularBooks(Integer offset, Integer limit){
+        List<Book> books = bookRepository.findAll();
+        for (Book book : books) {
+            double popularity = booksRatingAndPopulatityService.calculatePopularity(book.getId());
+            book.setPopularity(popularity);
+            bookRepository.save(book);
+        }
+        Pageable nextPage = PageRequest.of(offset,limit, Sort.by("popularity").descending());
+        return bookRepository.findAll(nextPage);
+    }
+
+    public Page<Book> getPageOfSearchResultBooks(String searchWord, Integer offset, Integer limit){
+        Pageable nextPage = PageRequest.of(offset,limit);
+        return bookRepository.findBookByTitleContaining(searchWord,nextPage);
+    }
+
+    public Page<Book> getPageTagsBooks(Integer offset, Integer limit) {
+        Pageable nextPage = PageRequest.of(offset, limit);
+        return bookRepository.findAll(nextPage);
+    }
+
+    public Page<Book> getPageTagsBooks(Integer tag_id, Integer offset, Integer limit) {
+        Pageable nextPage = PageRequest.of(offset, limit);
+        return bookRepository.findByTagId(tag_id, nextPage);
+    }
+
+    public List<Tag> getTags() {
+        List<Book> books = bookRepository.findAll();
+        List<Tag> tags = tagRepo.findAll();
+        List<Integer> counts = new ArrayList<>(Collections.nCopies(tags.size(), 0));
+        for (Book book : books) {
+            Integer tagIdInBook = book.getTag().getId() - 1;
+            counts.set(tagIdInBook, counts.get(tagIdInBook) + 1);
+        }
+        for(Tag tag: tags){
+            tag.setBook_count(counts.get(tag.getId()-1));
+            tagRepo.save(tag);
+        }
+        return tags;
+    }
+}
---
+++ skillbox/mod8/src/main/java/com/example/MyBookShopApp/security/AuthUserController.java
@@ -0,0 +1,121 @@
+package com.example.MyBookShopApp.security;
+
+import com.example.MyBookShopApp.errs.NoUserFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class AuthUserController {
+
+    private final BookstoreUserRegister userRegister;
+    private final BookstoreUserRepository bookstoreUserRepository;
+
+    @Autowired
+    public AuthUserController(BookstoreUserRegister userRegister,BookstoreUserRepository bookstoreUserRepository) {
+        this.userRegister = userRegister;
+        this.bookstoreUserRepository = bookstoreUserRepository;
+    }
+
+    @GetMapping("/signin")
+    public String handleSignin() {
+        return "signin";
+    }
+
+    @GetMapping("/signup")
+    public String handleSignUp(Model model) {
+        model.addAttribute("regForm", new RegistrationForm());
+        return "signup";
+    }
+
+    @PostMapping("/requestContactConfirmation")
+    @ResponseBody
+    public ContactConfirmationResponse handleRequestContactConfirmation(@RequestBody ContactConfirmationPayload contactConfirmationPayload) throws NoUserFoundException {
+        if(contactConfirmationPayload.getContact().contains("@")){
+            BookstoreUser bookstoreUser = bookstoreUserRepository.findBookstoreUserByEmail(contactConfirmationPayload.getContact());
+            if(bookstoreUser != null){
+                ContactConfirmationResponse response = new ContactConfirmationResponse();
+                response.setResult("true");
+                return response;
+            }
+            else{
+                throw new NoUserFoundException("Пользователя с такими данными не существует.");
+            }
+        }
+        else if(contactConfirmationPayload.getContact().contains("+")){
+            BookstoreUser bookstoreUser = bookstoreUserRepository.findBookstoreUserByPhone(contactConfirmationPayload.getContact());
+            if(bookstoreUser != null){
+                ContactConfirmationResponse response = new ContactConfirmationResponse();
+                response.setResult("true");
+                return response;
+            }
+            else{
+                throw new NoUserFoundException("Пользователя с такими данными не существует.");
+            }
+        } else{
+            throw new NoUserFoundException("Пользователя с такими данными не существует.");
+        }
+    }
+
+    @PostMapping("/approveContact")
+    @ResponseBody
+    public ContactConfirmationResponse handleApproveContact(@RequestBody ContactConfirmationPayload payload) {
+        ContactConfirmationResponse response = new ContactConfirmationResponse();
+        response.setResult("true");
+        return response;
+    }
+
+    @PostMapping("/reg")
+    public String handleUserRegistration(RegistrationForm registrationForm, Model model) {
+        userRegister.registerNewUser(registrationForm);
+        model.addAttribute("regOk", true);
+        return "signin";
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public ContactConfirmationResponse handleLogin(@RequestBody ContactConfirmationPayload payload,
+                                                   HttpServletResponse httpServletResponse) {
+
+        ContactConfirmationResponse loginResponse = userRegister.jwtLogin(payload);
+        Cookie cookie = new Cookie("token", loginResponse.getResult());
+        httpServletResponse.addCookie(cookie);
+        return loginResponse;
+    }
+
+    @GetMapping("/my")
+    public String handleMy() {
+        return "my";
+    }
+
+    @GetMapping("/profile")
+    public String handleProfile(Model model) {
+        model.addAttribute("curUsr", userRegister.getCurrentUser());
+        return "profile";
+    }
+
+//    @GetMapping("/logout")
+//    public String handleLogout(HttpServletRequest request) {
+//        HttpSession session = request.getSession();
+//        SecurityContextHolder.clearContext();
+//        if (session != null) {
+//            session.invalidate();
+//        }
+//
+//        for (Cookie cookie : request.getCookies()) {
+//            cookie.setMaxAge(0);
+//        }
+//
+//        return "redirect:/";
+//    }
+}
